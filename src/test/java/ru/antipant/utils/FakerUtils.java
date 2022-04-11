37b2f32f4bdb19a1c2d4279a3b8582468f34589e
CORP\aantipov <Alexander.Antipov@comindware.com>
2022-04-11 21:59:53 +0300

    Add private static variable faker

--- src/test/java/ru/antipant/utils/FakerUtils.java
+++ src/test/java/ru/antipant/utils/FakerUtils.java
@@ -4,24 +4,22 @@ import com.github.javafaker.*;
 
 public class FakerUtils {
 
+    private static final Faker faker = new Faker();
+
     public static String getFakerName() {
-        Faker faker = new Faker();
         return faker.starTrek().specie();
     }
 
     public static String getFakerEmail() {
-        Faker faker = new Faker();
         String emailDomain = "@qa.guru";
         return faker.pokemon().name() + emailDomain;
     }
 
     public static String getFakerSurName() {
-        Faker faker = new Faker();
         return faker.rickAndMorty().character();
     }
 
     public static String getFakerAddress() {
-        Faker faker = new Faker();
         return faker.funnyName().toString();
     }
 
